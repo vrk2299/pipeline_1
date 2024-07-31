1d5bad3b074f1b69bd56864d4698624b80802336
vrk2299 <93345302+vrk2299@users.noreply.github.com>
2024-07-31 14:24:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform {
+  backend "s3" {
+    bucket = "shailandco"
+    key    = "backend/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_server" {
+  instance_type        = "t2.micro"
+  ami                  = "ami-02a2af70a66af6dfb"
+  key_name             = "newone"
+  availability_zone    = "ap-south-1b"
+  hibernation          = true
+
+  root_block_device {
+    encrypted   = true
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "neekendukuuuu"
+  }
+
+  ebs_block_device {
+    device_name             = "/dev/sdb"
+    volume_size             = 8
+    encrypted               = true
+    delete_on_termination   = true
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${self.public_ip}, playbook.yml --private-key /path/to/your/private-key.pem"
+  }
+}
+
+output "aws_attributes" {
+  value = aws_instance.my_server
+}
