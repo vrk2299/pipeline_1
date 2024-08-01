fd7292fa9757f6aab3962b8e59ad582dd9c26312
vrk2299 <93345302+vrk2299@users.noreply.github.com>
2024-08-01 22:33:15 +0530

    Update playbook name tag  main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "my_server" {
   }
 
   provisioner "local-exec" {
-    command = "sleep 120 ; sudo ansible-playbook -i ${self.public_ip}, playbook.yml --private-key /tmp/keys/mum.pem"
+    command = "sleep 120 ; sudo ansible-playbook -i ${self.public_ip}, playbook.yaml --private-key /tmp/keys/mum.pem"
   }
 }
 
